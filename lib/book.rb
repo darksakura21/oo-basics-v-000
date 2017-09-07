c3158eeb3827266c099874b8f5dc649da34dfc83
Ruth Mesfun <ruth@flatironschool.com>
2017-09-07 00:14:11 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,12 @@
 class Book
   attr_acessor :author :page_count :genre
+
   def initialize(title)
     @title = title
   end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
 end
